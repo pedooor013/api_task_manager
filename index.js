c571b8e6ba16811a072c0f84ce92aeaba1328dc9
Pedro Lopes <pedrolopes.programador013@gmail.com>
2025-09-10 16:45:58 -0300

    addd: conexao com o servidor funcionando

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import {v4} from 'uuid';
 const port = 3000;
 const tasks = [];
 
-const server = http.createServer((request, response) =>{
+const server = http.createServer((request, res) =>{
     
     const {method, url} = request;
     let body = "";
@@ -14,7 +14,22 @@ const server = http.createServer((request, response) =>{
     
     request.on('end', () =>{
         const id = url.split('/')[2];
-        
-    })
 
+
+        //Criacao do metodo GET
+        if(url === "/grades" && method === "GET"){
+            res.writeHead(200, {'content-type': 'application/json'});
+            res.end(JSON.stringify(tasks));
+        }
+
+        //Caso a rota nao seja encontrada
+        else{
+            res.writeHead(404, {'content-type': 'application/json'});
+            res.end(JSON.stringify({message: 'Route not found'}));
+        }
+    });
 });
+
+server.listen(port, () =>{
+    console.log(`Server running in port ${port}`)
+})
